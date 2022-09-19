7f4fd220224b297e88dc683d7543292b942149fa
Kirill Mazinski <31702821+KirillMazinskiy@users.noreply.github.com>
2022-09-19 16:43:22 +0300

    Update and rename TASK_2.sql to TASK_2.1_A.sql

---
+++ SQL/TASK_2.1_A.sql
@@ -0,0 +1,52 @@
+WITH CALENDAR_OF_WORK_DATES AS ( 
+    SELECT 
+        T1_CAL.CALENDAR_DATE AS calendar_date,
+        CASE
+            WHEN WORK_DATES.fifth_work_date IS NULL     
+                 AND 
+                (LEAD(fifth_work_date,1,NULL) OVER(ORDER BY T1_CAL.CALENDAR_DATE)) IS NULL
+            THEN LEAD(fifth_work_date,2,NULL) OVER(ORDER BY T1_CAL.CALENDAR_DATE)
+            WHEN WORK_DATES.fifth_work_date IS NULL 
+                 AND 
+                (LEAD(fifth_work_date,1,NULL) OVER(ORDER BY T1_CAL.CALENDAR_DATE)) IS NOT NULL
+            THEN LEAD(fifth_work_date,1,NULL) OVER(ORDER BY T1_CAL.CALENDAR_DATE)
+            ELSE
+                 fifth_work_date
+        END fifth_work_date
+    FROM
+        CALENDAR T1_CAL
+    LEFT JOIN 
+            (
+                SELECT
+                        CALENDAR_DATE AS calendar_date,
+                        LEAD(CALENDAR_DATE,4,NULL) OVER(ORDER BY CALENDAR_DATE) AS fifth_work_date
+                FROM
+                        CALENDAR
+                WHERE
+                        IS_DAYOFF = 1
+             ) WORK_DATES
+    ON
+        T1_CAL.CALENDAR_DATE = WORK_DATES.calendar_date
+        )
+  
+SELECT
+    T1_MESSAGE.DOCUMENT_ID AS doc_id 
+FROM 
+    MESSAGE T1_MESSAGE  
+INNER JOIN 
+    (
+        SELECT 
+                DOCUMENT_ID AS CHILD_DOCUMENT_ID, 
+                DOCUMENT_DT AS answer_dt
+        FROM
+                MESSAGE
+    ) T2_MESSAGE 
+ON 
+    T1_MESSAGE.CHILD_DOCUMENT_ID  = T2_MESSAGE.CHILD_DOCUMENT_ID
+LEFT  JOIN 
+        CALENDAR_OF_WORK_DATES
+ON 
+        T1_MESSAGE.DOCUMENT_DT  = CALENDAR_OF_WORK_DATES.calendar_date
+WHERE      
+        T2_MESSAGE.answer_dt > CALENDAR_OF_WORK_DATES.fifth_work_date
+   
