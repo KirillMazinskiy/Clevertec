7695f011d5e37e02158411215200469174124fcd
Kirill Mazinski <31702821+KirillMazinskiy@users.noreply.github.com>
2022-09-19 17:00:33 +0300

    Update TASK_1.sql

--- SQL/TASK_1.sql
+++ SQL/TASK_1.sql
@@ -1,2 +1,21 @@
-SELECT * 
-FROM TABLE_1
+SELECT DISTINCT 
+    (SELECT
+         COUNT(product_id) 
+     FROM    
+         PRODUCTS 
+     WHERE
+         cost < 100) AS products_with_cost_less_100, 
+    (SELECT
+         COUNT(product_id) 
+     FROM
+         PRODUCTS
+     WHERE
+         cost BETWEEN 100 AND 200) AS products_with_cost_between_100_and_200,
+    (SELECT
+         COUNT(product_id) 
+     FROM 
+         PRODUCTS 
+     WHERE cost > 200) AS products_with_cost_more_200
+FROM
+    PRODUCTS
+
